a9e9e929dd18dde6473a57abaa38fee0b7f93660
Mathew Aboujaoude <mathewmadmen@gmail.com>
2025-06-05 19:58:10 +0300

    v0.1.1 – added song dictionary storage and CLI setlist view

---
+++ src/addsongs.py
@@ -0,0 +1,34 @@
+class Song: #class to store info of new songs
+    def __init__ (self, title, artist, album, key, tuning):
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.key = key
+        self.tuning = tuning
+
+    def to_dict(self): #converts song info to a k, v pair
+        return {
+            'title': self.title,
+            'artist': self.artist,
+            'album': self.album,
+            'key': self.key,
+            'tuning': self.tuning
+        }
+
+    def __str__(self): #prints full song info when {new} is called
+        return (f'''
+New Song Added!
+Song Title: {self.title}
+Artist: {self.artist}
+Album: {self.album}
+Key: {self.key}
+Tuning: {self.tuning}
+''')
+    
+def new_song(): # asks user for info on song they're adding
+    title = input('Please enter the song title: ').title().strip()
+    artist = input('Please enter the artist name: ').title().strip()
+    album = input('Please enter the album name: ').title().strip()
+    key = input('Please enter the key of the song: ').capitalize().strip() #capitlize only in case key is X#m (minor)
+    tuning = input('Please enter the tuning used for the song: ').title().strip()
+    return Song(title, artist, album, key, tuning)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,37 @@
 from version import __version__
+from addsongs import Song, new_song
+
+songs = []
 
 def main():
     print(f'Setlist Manager version v{__version__}')
+    user_input() #starts menu loop and asks for user input
+
+def user_input():
+    while True:
+        print('''
+              Main Menu
+    (new setlist/add song/view setlist)
+              ''')
+        user_input = input('Choose one of the menu options: ').lower()
+        if user_input == 'add song':
+            new = new_song()
+            songs.append(new.to_dict()) 
+            print(f'\n{new}') #prints full song info
+        elif user_input == 'view setlist':
+            print('\nSetlist Tracks:')
+            for song in songs:
+                print(f"- {song['title']} by {song['artist']}")
+        elif user_input == 'new setlist':
+            pass
+        else:
+            print('Please choose a valid input')
+
+class Setlist: #unsued at the moment, will need it's own module
+    def __init__ (self, name, venue, date):
+        self.name = name
+        self.venue = venue
+        self.date = date
 
 if __name__ == '__main__':
     main()
